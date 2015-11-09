69b12491ca1c958a985483b66f7554678483db80
andruzko <andruzko@gmai.com>
2015-11-09 14:46:09 +0200

    add searching

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
   validates :title, :body, :tag, presence: true
   validates :title, uniqueness: true
   validates :title, length: { in: 5..100 }
+
+  def self.search(query)
+  # where(:title, query) -> This would return an exact match of the query
+   where("title like ? or body like ?", "%#{query}%", "%#{query}%")
+  end
 end
